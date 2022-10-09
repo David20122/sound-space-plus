f36917a1c6ef207f590b3c7d9ea4040d078d60a5
Chedski <chedski@laurusgames.com>
2022-10-09 13:26:48 -0400

    fix(menu): Folder buttons cause crashes on Android
    The "Open Replay Folder" button is hidden on Android
    The "Open User Folder" button is disabled and shows the user folder path on Android

--- menuButtons/UserDir.gd
+++ menuButtons/UserDir.gd
@@ -1,4 +1,10 @@
 extends Button
 
 func _pressed():
-	OS.shell_open(ProjectSettings.globalize_path(Globals.p("user://")))
+	if !OS.has_feature("Android"):
+		OS.shell_open(ProjectSettings.globalize_path(Globals.p("user://")))
+
+func _ready():
+	if OS.has_feature("Android"):
+		disabled = true
+		text = "User folder location: " + Globals.p("user://")
