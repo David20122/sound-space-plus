4a1a72ce5b487d2df4368df3797a8debd1ffaf8e
Gapva <90116898+Gapva@users.noreply.github.com>
2023-01-24 16:39:07 -0500

    small tweaks

--- VersionNumber.gd
+++ VersionNumber.gd
@@ -1,4 +1,4 @@
 extends Label
 
 func _ready():
-	text = "Sound Space Plus v%s" % ProjectSettings.get_setting("application/config/version")
+	text = "Sound Space Plus [%s]" % ProjectSettings.get_setting("application/config/version")
