929f21a523190fa095ebbd19c3e237daa2bc1064
jayden <46307433+krmeet@users.noreply.github.com>
2023-02-05 17:47:58 +0000

    create Solo Scene

--- scripts/game/GameScene.gd
+++ scripts/game/GameScene.gd
@@ -1,7 +1,10 @@
-extends Node
+extends Spatial
 class_name GameScene
 
 export(NodePath) var sync_manager
 export(NodePath) var note_manager
 export(NodePath) var hud_manager
 export(NodePath) var camera
+
+func _ready():
+	sync_manager.get_node()
