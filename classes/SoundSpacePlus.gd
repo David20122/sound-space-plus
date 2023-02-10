25cfc149966bbd61377d8104c32b85fc728cead5
David20122 <wilczekhd@op.pl>
2023-02-10 21:41:16 +0100

    add sex

--- classes/SoundSpacePlus.gd
+++ classes/SoundSpacePlus.gd
@@ -706,6 +706,7 @@ var do_note_pushback:bool = true # true; notes go past grid on miss, false; note
 var show_stats:bool = false
 
 var arcw_mode:bool = false # heheheha
+var sex_mode:bool = false # demon mode
 
 
 
@@ -2300,6 +2301,10 @@ func do_init(_ud=null):
 	if Input.is_key_pressed(KEY_A) and Input.is_key_pressed(KEY_R) and Input.is_key_pressed(KEY_C) and Input.is_key_pressed(KEY_W):
 		arcw_mode = true
 		alert = "ARCW mode enabled successfully."
+	if Input.is_key_pressed(KEY_S) and Input.is_key_pressed(KEY_E) and Input.is_key_pressed(KEY_X):
+		# we do a little trolling
+		sex_mode = true
+		alert = "Sex mode enabled successfully."
 	
 	var alert_snd_played:bool = false
 	if alert != "":
