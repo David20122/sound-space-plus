b912ed682b4723e34a16bcae94fe64472e423929
jayden <46307433+krmeet@users.noreply.github.com>
2023-02-26 23:15:20 +0000

    create mods class

---
+++ scripts/game/Mods.gd
@@ -0,0 +1,19 @@
+extends Object
+class_name Mods
+
+enum Speed {
+	PRESET,
+	CUSTOM
+}
+const SpeedPresets = [
+	1,
+	1.0/1.35,
+	1.0/1.25,
+	1.0/1.15,
+	1.15,
+	1.25,
+	1.35
+]
+var speed_mod = Speed.PRESET
+var speed_preset = 0
+var speed_custom = 1
