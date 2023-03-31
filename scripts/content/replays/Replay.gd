91acf0fab24d88fe23aa80cc5745262023e65c6c
jayden <46307433+krmeet@users.noreply.github.com>
2023-03-31 10:00:46 +0100

    start working on replay stuff (at shol)

--- scripts/content/replays/Replay.gd
+++ scripts/content/replays/Replay.gd
@@ -2,3 +2,15 @@ extends ResourcePlus
 class_name Replay
 
 const SIGNATURE:PackedByteArray = [0x53,0x73,0x2A,0x52]
+
+var frames:Array[Frame]
+
+class Frame:
+	enum Type {
+		CURSOR,
+		HIT
+	}
+	var index:int
+	var type:int
+	var position:Vector2
+	var note_index:int
