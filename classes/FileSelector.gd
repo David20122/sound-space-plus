62867c8ba168120091f690f7dec9690f926a0fda
David20122 <wilczekhd@op.pl>
2023-04-26 01:37:23 +0200

    max openfile dialog scale = 1

--- classes/FileSelector.gd
+++ classes/FileSelector.gd
@@ -72,7 +72,7 @@ func open_file(
 	target_method = method
 	
 	$C/OpenFile.rect_pivot_offset = get_viewport_rect().size / 2
-	$C/OpenFile.rect_scale = Vector2(SSP.render_scale, SSP.render_scale)
+	$C/OpenFile.rect_scale = Vector2(clamp(SSP.render_scale,0.5,1), clamp(SSP.render_scale,0.5,1))
 	
 	if use_native:
 		$OpenFile.filters = filters
