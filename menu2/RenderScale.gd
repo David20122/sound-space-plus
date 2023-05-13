ed3fec87fd05956649de589ac74a51e6f1c7348c
David20122 <wilczekhd@op.pl>
2023-05-13 02:32:16 +0200

    use get_screen_size in renderscale

--- menu2/RenderScale.gd
+++ menu2/RenderScale.gd
@@ -1,12 +1,12 @@
 extends SpinBox
 
 func _on_Scale_value_changed(value):
-	var resolution = OS.window_size * value
+	var resolution = OS.get_screen_size() * value
 	SSP.render_scale = value
 	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_EXPAND, resolution)
 
 func viewport_size_changed():
-	var resolution = OS.window_size * SSP.render_scale
+	var resolution = OS.get_screen_size() * SSP.render_scale
 	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_EXPAND, resolution)
 	
 func _ready():
