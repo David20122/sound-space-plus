7ee2974c4fdec2edeff60cb5656f388874119e00
David20122 <wilczekhd@op.pl>
2023-05-13 03:32:17 +0200

    even better c:

--- classes/Replay.gd
+++ classes/Replay.gd
@@ -3,7 +3,7 @@ class_name Replay
 
 const file_sig:PoolByteArray = PoolByteArray([0x53,0x73,0x2A,0x52])
 const current_sv:int = 4
-const debug:bool = false
+var debug:bool = OS.has_feature("debug")
 
 var dance:DanceMover
 var song:Song
