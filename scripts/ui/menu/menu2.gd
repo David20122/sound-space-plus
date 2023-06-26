14be4ea64e10b36a5f05ee08ea86d6a16aabce73
jayden <46307433+krmeet@users.noreply.github.com>
2023-06-26 21:35:49 +0100

    fix it proerply

--- scripts/ui/menu/menu2.gd
+++ scripts/ui/menu/menu2.gd
@@ -14,7 +14,6 @@ func idle_status():
 
 func _ready():
 	get_tree().paused = false
-#	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_DISABLED, SceneTree.STRETCH_ASPECT_IGNORE, OS.window_size * SSP.render_scale)
 	if SSP.arcw_mode:
 		get_tree().change_scene("res://w.tscn")
 	if SSP.sex_mode:
--- scripts/ui/settings/RenderScale.gd
+++ scripts/ui/settings/RenderScale.gd
@@ -3,13 +3,13 @@ extends SpinBox
 func _on_Scale_value_changed(value):
 	var resolution = OS.window_size * value
 	SSP.render_scale = value
-	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_VIEWPORT, SceneTree.STRETCH_ASPECT_IGNORE, resolution)
+	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_IGNORE, resolution)
 
 func viewport_size_changed():
 	var resolution = OS.window_size * SSP.render_scale
-	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_VIEWPORT, SceneTree.STRETCH_ASPECT_IGNORE, resolution)
+	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_IGNORE, resolution)
 	
 func _ready():
-	 get_tree().get_root().connect("size_changed", self, "viewport_size_changed")
-	 connect("changed", self, "_on_Scale_value_changed")
-	 value = SSP.render_scale
+	get_tree().get_root().connect("size_changed", self, "viewport_size_changed")
+	connect("changed", self, "_on_Scale_value_changed")
+	value = SSP.render_scale
