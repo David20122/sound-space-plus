e89fd392b489efc9389a1ada1f378c381e3a30bf
David20122 <wilczekhd@op.pl>
2023-11-21 00:12:25 +0100

    start offset changes

--- scripts/ui/menu/StartOffset.gd
+++ scripts/ui/menu/StartOffset.gd
@@ -9,17 +9,27 @@ func value_changed(value):
 
 func on_map_selected(map):
 	self.max_value = get_seconds_from_ms(Rhythia.selected_song.last_ms)
-
+	self.value = 0
+	
 func _ready():
 	connect("value_changed",self,"value_changed")
+	$TimeTextBox.connect("text_entered", self, "time_text_entered")
 	Rhythia.connect("selected_song_changed",self,"on_map_selected")
 	
 	if (Rhythia.selected_song != null):	# after song pass
 		on_map_selected(null)
 		self.value = Rhythia.start_offset / 1000
 		
+func time_text_entered(new_text):
+	var time = new_text.split(':',false,1)
+	var total_seconds:int
+	
+	if time.size() == 2: total_seconds = int(time[0]) * 60 + int(time[1])
+	else: total_seconds = int(time[0])
+	self.value = total_seconds
+		
 func upd_label():
 	var total_seconds = int(self.value)
 	var minutes = floor(total_seconds / 60)
 	var seconds = total_seconds % 60
-	$TimeLabel.text = "%d:%02d" % [minutes,seconds]
+	$TimeTextBox.text = "%d:%02d" % [minutes,seconds]
