3642db627846e5ce81f49edbe1eca92196c901c4
NoiceBroice <bricesanford@gmail.com>
2024-04-29 23:25:31 -0400

    fix search online inconsistency

--- scripts/ui/menu/SearchOnline.gd
+++ scripts/ui/menu/SearchOnline.gd
@@ -1,13 +1,21 @@
 extends CheckBox
 
+export(String) var target
+
+func upd():
+#	print('scb "%s"' % name)
+	pressed = Rhythia.get(target)
+
 func _pressed():
+	if pressed != Rhythia.get(target):
+		Rhythia.set(target,pressed)
 	get_parent().get_parent().get_node("MapRegistry/S/VBoxContainer").update_search_showonline(
-		!get_parent().get_parent().get_node("MapRegistry/S/VBoxContainer").show_online_maps
+		Rhythia.last_search_incl_online
 	)
 	Rhythia.last_search_incl_online = pressed
 
 func _ready():
-	pressed = Rhythia.last_search_incl_online
+	upd()
 	get_parent().get_parent().get_node("MapRegistry/S/VBoxContainer").update_search_showonline(
 		Rhythia.last_search_incl_online
 	)
