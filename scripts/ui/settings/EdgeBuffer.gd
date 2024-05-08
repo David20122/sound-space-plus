aa8559377f745a5e4c38c4d49967faa5eb59396a
mycpphurts <defaltlegal@gmail.com>
2024-05-08 13:12:56 +0100

    Hard Rock edge buffer thing

--- scripts/ui/settings/EdgeBuffer.gd
+++ scripts/ui/settings/EdgeBuffer.gd
@@ -1,11 +1,21 @@
 extends SpinBox
 
+var ov
+
 func upd():
-	Rhythia.edge_drift = value
+	if Rhythia.mod_hardrock:
+		Rhythia.edge_drift = ((value + 1) * 1.6) - 1
+	else:
+		Rhythia.edge_drift = ov
 
 func _process(_d):
 	if value != Rhythia.edge_drift: upd()
 
+func _on_Rhythia_mods_changed():
+	upd()
+
 func _ready():
+	ov = value
 	value = Rhythia.edge_drift
-	connect("changed",self,"upd")
+	connect("changed", self, "upd")
+	Rhythia.connect("mods_changed", self, "_on_Rhythia_mods_changed")
