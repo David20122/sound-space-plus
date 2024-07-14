a019658fa46fb1861550ed7e88a30d45e8139d4d
mycpphurts <defaltlegal@gmail.com>
2024-07-15 00:15:21 +0100

    Revert EdgeBuffer to pre-hardrock

--- scripts/ui/settings/EdgeBuffer.gd
+++ scripts/ui/settings/EdgeBuffer.gd
@@ -1,25 +1,11 @@
 extends SpinBox
 
-var ov
-
 func upd():
-	if Rhythia.mod_hardrock:
-		var nv = ((value + 1) * 1.6) - 1
-		Rhythia.edge_drift = nv
-	else:
-		Rhythia.edge_drift = ov
+	Rhythia.edge_drift = value
 
 func _process(_d):
-	if value != Rhythia.edge_drift and Rhythia.mod_hardrock:
-		return
-	elif value != Rhythia.edge_drift and !Rhythia.mod_hardrock:
-		upd()
-
-func _on_Rhythia_mods_changed():
-	upd()
+	if value != Rhythia.edge_drift: upd()
 
 func _ready():
-	ov = value
 	value = Rhythia.edge_drift
-	connect("changed", self, "upd")
-	Rhythia.connect("mods_changed", self, "_on_Rhythia_mods_changed")
+	connect("changed",self,"upd")
