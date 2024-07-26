ab1fe52c71442e663c2db665d2e4a6c0f8b31386
mycpphurts <defaltlegal@gmail.com>
2024-07-26 01:38:36 +0100

    Chore: More readable Failed MapDB connection

--- scripts/Online.gd
+++ scripts/Online.gd
@@ -145,7 +145,7 @@ func load_db_maps():
 		if !yield(self,"_connection_test"):
 			Globals.notify(
 				Globals.NOTIFY_ERROR,
-				"Online maps will not be loaded - failed to connect.\n\nMake sure your system clock is Synchronized!",
+				"Online maps will not be loaded - failed to connect to map database.\n\nMake sure your system clock is Synchronized!",
 				"No Connection"
 			)
 			emit_signal("db_maps_done")
